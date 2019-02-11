3f5cae83aca967df67a71008e33f8840951b91da
kingkong456 <peeradechwomkum35186@gmail.com>
2019-02-11 15:57:42 +0700

    Update

---
+++ ThelostToysV3/Assets/Srcipt/Ui/Tutorial.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour {
+    public GameObject[] tutorial_panel;
+    private int currentPanel = -1;
+    public GameObject tutorialPanel;
+
+    private void Start()
+    {
+        nextpanel();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButtonDown("all_j_buttun_summit"))
+        {
+            tutorial_panel[currentPanel].SetActive(false);
+            tutorialPanel.SetActive(false);
+        }
+    }
+
+    public void nextpanel()
+    {
+        currentPanel++;
+        tutorial_panel[currentPanel].SetActive(true);
+    }
+}
---
+++ ThelostToysV3/Assets/Srcipt/Ui/tutorialTriiger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialTriiger : MonoBehaviour {
+
+    void OnTrigerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            GameObject.FindObjectOfType<Tutorial>().nextpanel();
+        }
+    }
+
+}
